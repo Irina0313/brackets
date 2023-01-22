310a5a3b4319c766f792344fbb6503d48402e5ee
Irina0313 <irkinbk@gmail.com>
2023-01-23 02:56:15 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function towelSort(matrix, par) {
+  matrix = matrix.split('');
+  function reduseSimilar() {
+    for (let i = 0; i < matrix.length; i++) {
+      par.forEach(element => {
+        if (element[0] == matrix[i] && element[1] == matrix[i + 1]) {
+          matrix.splice(i, 2);
+          if (matrix.length > 0) {
+            reduseSimilar();
+          }
+        }
+      });
+    }
+    return matrix;
+  }
+  reduseSimilar();
+  if (matrix.length > 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
